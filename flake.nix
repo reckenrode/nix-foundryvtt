1b9ae7c4f86ffadc8de22dd3981209ab021eafae
Randy Eckenrode <randy@largeandhighquality.com>
2024-06-17 18:22:20 -0400

    Update flake nixpkgs to 24.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     the browser.
   '';
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
   outputs =
     inputs@{ self, nixpkgs, ... }:
