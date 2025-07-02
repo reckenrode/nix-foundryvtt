71c505c180d5b3551bb8892983f2f447e049f880
Randy Eckenrode <randy@largeandhighquality.com>
2025-07-01 20:58:25 -0400

    nixpkgs: 24.11 → 25.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     the browser.
   '';
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
   outputs =
     { self, nixpkgs, ... }:
